edf4647777b07cfe5f6162a3534cd05cf3592116
ryota koyama <kryota.gin@gmail.com>
2018-04-01 19:24:57 +0900

    不要なコード削除

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,6 @@
   $page = ((int)$_GET['page'] > 0) ? (int)$_GET['page'] : 1;
 
   $stmt = $pdo->query('select count(*) from product');
-  $stmt->execute();
   $total = $stmt->fetchColumn();
   $hasNext = ($page * 4 < $total);
   $hasPrevious = ($page >= 2);
@@ -13,7 +12,6 @@
 
   $offset = ($page - 1) * 4;
   $sql = $pdo->query("select * from product order by id desc limit {$offset}, 4");
-  $sql->execute();
   $products = $sql->fetchAll();
-  
+
   require_once(dirname(__DIR__).'/public_html/view/index.php');
